b0f850a917679ce7ec06f16f9b2d62ea8b17eb9a
juliabazanella0 <julia.bazanella0@gmail.com>
2021-05-24 22:58:34 -0300

    Implementa bottomNavigation

--- app/src/main/java/com/example/trabalho_final/MainActivity.kt
+++ app/src/main/java/com/example/trabalho_final/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.trabalho_final
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.fragment_config_game.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.setupWithNavController(findNavController(R.id.navHostFragment))
     }
+
+//    fun goToLogin() {
+//        var intent = Intent(this, LoginActivity::class.java).apply {
+//            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+//        }
+//
+//        startActivity(intent)
+//    }
+
+//    fun logout() {
+//        this.getSharedPreferences("game", Context.MODE_PRIVATE)
+//                .edit().apply {
+//                    clear()
+//                    commit()
+//                }
+//        this.getSharedPreferences("config", Context.MODE_PRIVATE)
+//                .edit().apply {
+//                    clear()
+//                    commit()
+//                }
+//        this.getSharedPreferences("login", Context.MODE_PRIVATE)
+//                .edit().apply {
+//                    clear()
+//                    commit()
+//                }
+//
+//        goToLogin()
+//    }
 }
