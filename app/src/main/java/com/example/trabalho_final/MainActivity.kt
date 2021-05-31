e77cf350d323f2a208093d1f95aa41046be13bbb
juliabazanella0 <julia.bazanella0@gmail.com>
2021-05-31 17:05:03 -0300

    Logout concluido

--- app/src/main/java/com/example/trabalho_final/MainActivity.kt
+++ app/src/main/java/com/example/trabalho_final/MainActivity.kt
@@ -3,19 +3,21 @@ package com.example.trabalho_final
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.fragment_config_game.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MenuItem.OnMenuItemClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bottomNavigationView.setupWithNavController(findNavController(R.id.navHostFragment))
+        bottomNavigationView.menu.getItem(1).setOnMenuItemClickListener(this)
     }
 
-    /**fun goToLogin() {
+    fun goToLogin() {
         var intent = Intent(this, LoginActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         }
@@ -24,22 +26,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun logout() {
-        this.getSharedPreferences("game", MODE_PRIVATE)
-                .edit().apply {
+        this.getSharedPreferences("game", MODE_PRIVATE).edit().apply {
                     clear()
                     commit()
                 }
-        this.getSharedPreferences("config", MODE_PRIVATE)
-                .edit().apply {
+        this.getSharedPreferences("config", MODE_PRIVATE).edit().apply {
                     clear()
                     commit()
                 }
-        this.getSharedPreferences("login", MODE_PRIVATE)
-                .edit().apply {
+        this.getSharedPreferences("login", MODE_PRIVATE).edit().apply {
                     clear()
                     commit()
                 }
 
         goToLogin()
-    }**/
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.btLogout){
+            logout()
+        }
+        return true
+    }
 }
